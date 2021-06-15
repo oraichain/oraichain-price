7e8aed74fee9bd4e32a420d02dd3c295b1cc2878
Le Duc Pham <ducphamle212@gmail.com>
2021-06-15 19:26:31 +0700

    init server calling ai requests

---
+++ app.js
@@ -0,0 +1,19 @@
+import { setAiRequest } from './utils';
+
+const runPriceFeed = async () => {
+  const oracleScriptName = process.env.ORACLE_SCRIPT || "oscript_price_special";
+  const count = parseInt(process.env.COUNT) || 1;
+  try {
+    await setAiRequest(oracleScriptName, count);
+  } catch (error) {
+    console.log("error: ", error)
+
+  }
+  setTimeout(runPriceFeed, 120000);
+}
+
+const start = async () => {
+  runPriceFeed();
+};
+
+start();
---
+++ utils.js
@@ -0,0 +1,57 @@
+import Cosmos from '@oraichain/cosmosjs';
+import KSUID from 'ksuid';
+import Long from 'long';
+import bech32 from 'bech32';
+import fs from 'fs';
+import util from 'util';
+const log_file = fs.createWriteStream(__dirname + '/debug.log', { flags: 'w' });
+const log_stdout = process.stdout;
+
+const config = {};
+if (process.env.NODE_ENV !== 'production') {
+    config.path = `.env.${process.env.NODE_ENV}`;
+}
+require('dotenv').config(config);
+
+const message = Cosmos.message;
+const cosmos = new Cosmos(process.env.LCD_URL, process.env.CHAIN_ID);
+cosmos.setBech32MainPrefix('orai');
+const childKey = cosmos.getChildKey(process.env.MNEMONIC);
+
+export const setAiRequest = async (oscriptName, count) => {
+    const req_id = KSUID.randomSync().string;
+    // get accAddress in binary
+    const fees = process.env.FEES || '1orai';
+    const input = '';
+    const expectedOutput = '';
+    const accAddress = bech32.fromWords(bech32.toWords(childKey.identifier));
+    const msgSend = new message.oraichain.orai.airequest.MsgSetAIRequest({
+        request_id: req_id,
+        oracle_script_name: oscriptName,
+        creator: accAddress,
+        validator_count: new Long(count),
+        fees: fees ? fees : '0orai',
+        input: Buffer.from(input),
+        expected_output: Buffer.from(expectedOutput)
+    });
+
+    const msgSendAny = new message.google.protobuf.Any({
+        type_url: '/oraichain.orai.airequest.MsgSetAIRequest',
+        value: message.oraichain.orai.airequest.MsgSetAIRequest.encode(msgSend).finish()
+    });
+
+    const txBody = new message.cosmos.tx.v1beta1.TxBody({
+        messages: [msgSendAny],
+        memo: ''
+    });
+
+    try {
+        const response = await cosmos.submit(childKey, txBody, 'BROADCAST_MODE_BLOCK', 0, 300000);
+        console.log(response);
+        if (response.tx_response.code !== 0) throw Error(response.tx_response.raw_log);
+        console.log('request id: ', req_id);
+    } catch (ex) {
+        log_file.write(`timestamp: ${new Date()} with ${util.format(ex)}` + '\n');
+        log_stdout.write(`timestamp: ${new Date()} with ${util.format(ex)}` + '\n');
+    }
+}
