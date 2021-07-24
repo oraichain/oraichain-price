364666e1732498888285a674d6ba42208713f274
Duc Pham Le <ducphamle212@gmail.com>
2021-07-24 19:07:09 +0700

    add runtime transform babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
+/* eslint global-require: off */
+
 module.exports = (api) => {
     api.cache(true);
     return {
         presets: ['@babel/preset-flow'],
+        plugins: ['@babel/plugin-transform-runtime']
     };
 };
